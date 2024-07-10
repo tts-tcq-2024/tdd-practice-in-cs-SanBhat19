add030948cb5c2e80e69d34aa60381ce427d54d4
Sanjay Subrahmanya Bhat <sanq1kor@bosch.com>
2024-07-10 10:54:48 +0530

    commit

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,1 +1,41 @@
+using System;
+using System.Collections.Generic;
 
+public class StringCalculator
+{
+    public int Add(string numbers)
+    {
+        if (string.IsNullOrEmpty(numbers))
+        {
+            return 0;
+        }
+
+        char[] delimiters = new char[] { ',', '\n' };
+        string[] splitNumbers = numbers.Split(delimiters, StringSplitOptions.None);
+
+        if (numbers.StartsWith("//"))
+        {
+            int delimiterIndex = numbers.IndexOf('\n');
+            string customDelimiter = numbers.Substring(2, delimiterIndex - 2);
+            splitNumbers = numbers.Substring(delimiterIndex + 1).Split(new string[] { customDelimiter }, StringSplitOptions.None);
+        }
+
+        List<int> numList = new List<int>();
+        foreach (string number in splitNumbers)
+        {
+            if (int.TryParse(number, out int parsedNumber))
+            {
+                if (parsedNumber < 0)
+                {
+                    throw new Exception("Negatives not allowed");
+                }
+                if (parsedNumber <= 1000)
+                {
+                    numList.Add(parsedNumber);
+                }
+            }
+        }
+
+        return numList.Sum();
+    }
+}
